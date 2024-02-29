c0921efa61a55bd4bb7da6eb7780608dbe6536bc
ObedGyawu <obedgyawu84@gmail.com>
2024-02-28 22:39:46 -0500

    "Hello"

---
+++ BasicCalculator.py
@@ -0,0 +1,48 @@
+# Create a simple calculator that can perform basic arithmetic
+# operations like addition, subtraction, multiplication, and division.
+
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Can be divided by zero"
+
+
+def calculator():
+    print("select your choice of operation")
+    print("1.Add")
+    print("2.Subtract")
+    print("3.Multiply")
+    print("4.Divide")
+
+    selection = input("Enter your choice: ")
+
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+
+    if selection == '1':
+        result = add(num1, num2)
+    elif selection == '2':
+        result = subtract(num1, num2)
+    elif selection == '3':
+        result = multiply(num1, num2)
+    elif selection == '4':
+        result = divide(num1, num2)
+    else:
+        result = "Invalid"
+    print(f"Result: {result}")
+
+
+calculator()
